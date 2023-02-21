8e22e257422c42a251c4108dffd78bc11df5b415
TesfawTsion Shimelis <bebishuteda@gmail.com>
2023-02-21 12:14:52 +0300

    profile_page_added

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,12 +1,223 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:property/Constants/Colors.dart';
+import 'package:property/Widgets/headline.dart';
+import 'package:property/Widgets/headline1.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('profile')),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              height: 15.w,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Profile',
+                  style: GoogleFonts.lato(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w700,
+                      height: 1.3),
+                ),
+                SizedBox(
+                  width: 150.w,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 2.w, horizontal: 2.w),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(100)),
+                  child: Icon(
+                    Icons.more_horiz_rounded,
+                    size: 20.sp,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 40.w,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'lib/images/p2.jpg',
+                    ),
+                    radius: 50.sp,
+                  ),
+                  Positioned(
+                      top: 75.w,
+                      left: 75.w,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          color: kPrimaryColor,
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 2.w, vertical: 2.w),
+                        child: Center(
+                          child: Icon(
+                            Icons.edit,
+                            size: 20.sp,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ))
+                ]),
+                Text(
+                  'Bethelihem Mengesha',
+                  style: GoogleFonts.lato(
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600,
+                      height: 1.4.sp),
+                ),
+                Text(
+                  'Bety2022@gmail.com',
+                  style: GoogleFonts.lato(
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w400,
+                      height: 1.4.sp),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 10.w),
+              child: Divider(
+                thickness: 1.2.w,
+              ),
+            ),
+            proList(
+              icon: Icons.person_outline_rounded,
+              text: 'Edit Profile',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.notifications_active_outlined,
+              text: 'Notification',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.payment_outlined,
+              text: 'Payment',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.security_outlined,
+              text: 'Security',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.language,
+              text: 'Language',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.privacy_tip_rounded,
+              text: 'Privacy',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.help_outline_sharp,
+              text: 'Help Center',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.groups_outlined,
+              text: 'Invite friend',
+              ontap: () {},
+            ),
+            SizedBox(
+              height: 15.w,
+            ),
+            proList(
+              icon: Icons.help_outline_sharp,
+              text: 'Help Center',
+              ontap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class proList extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final VoidCallback ontap;
+  const proList({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.ontap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: GestureDetector(
+        onDoubleTap: ontap,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Wrap(
+              children: [
+                Icon(
+                  icon,
+                  size: 24.sp,
+                ),
+                SizedBox(
+                  width: 10.w,
+                ),
+                Text(
+                  text,
+                  style: GoogleFonts.roboto(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      height: 1.28.w),
+                ),
+              ],
+            ),
+            Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 22.sp,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
